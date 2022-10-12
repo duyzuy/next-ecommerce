fd713864342975f324a315b19349bf0b0a7d7aa4
Duy Vu <vutruongduy2109@gmail.com>
2022-10-12 21:57:46 +0700

    update

---
+++ components/RateStars/index.jsx
@@ -0,0 +1,6 @@
+import * as Icon from 'semantic-ui-react';
+import { makeArrayFromLength } from '../../utils/helper';
+const RateStars = ({ stars = 5, rating }) => {
+  const arrStar = makeArrayFromLength(stars);
+};
+export default RateStars;
